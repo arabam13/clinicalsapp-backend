2e1e6b3021029132b3bd11bbd6bbc9cb5fbec3e1
araba <araba.mohamed@gmail.com>
2024-07-28 17:15:58 +0200

    Creating Patient Controller

--- src/main/java/com/clinical/clinicalapi/controllers/PatientController.java
+++ src/main/java/com/clinical/clinicalapi/controllers/PatientController.java
@@ -1,47 +1,40 @@
 package com.clinical.clinicalapi.controllers;
-// package com.clinical.api.endpoints;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.clinical.api.models.Patient;
-// import com.clinical.api.repositories.PatientRepo;
-
-// import jakarta.ws.rs.Consumes;
-// import jakarta.ws.rs.GET;
-// import jakarta.ws.rs.POST;
-// import jakarta.ws.rs.Path;
-// import jakarta.ws.rs.PathParam;
-// import jakarta.ws.rs.Produces;
+import com.clinical.clinicalapi.models.Patient;
+import com.clinical.clinicalapi.repositories.PatientRepo;
 
 
-// @Consumes("application/json")
-// @Produces("application/json")
-// @Path("/api")
-// public class PatientService {
+@RestController
+@RequestMapping("/api")
+public class PatientController {
 
-//     @Autowired
-//     PatientRepo patientRepo;
+    @Autowired
+    PatientRepo patientRepo;
 
-//     @Path("/patients")
-//     @GET
-//     public List<Patient> getPatients() {
-//         System.out.println("##########################################");
-//         System.out.println("GET PATIENTS");
-//         System.out.println("##########################################");
-//         return patientRepo.findAll();
-//     }
+    @GetMapping("/patients")
+    public List<Patient> getPatients() {
+        System.out.println("##########################################");
+        System.out.println("GET PATIENTS");
+        System.out.println("##########################################");
+        return patientRepo.findAll();
+    }
 
-//     @Path("/patients")
-//     @POST
-//     public Patient createPatient(Patient patient) {
-//         return patientRepo.save(patient);
-//     }
+    @PostMapping("/patients")
+    public Patient createPatient(Patient patient) {
+        return patientRepo.save(patient);
+    }
 
-//     @Path("/patients/{id}")
-//     @GET
-//     public Patient getPatient(@PathParam("id") Integer id) {
-//         return patientRepo.findById(id).get();
-//     }
-// }
+    @GetMapping("/patients/{id}")
+    public Patient getPatient(@PathVariable("id") Integer id) {
+        return patientRepo.findById(id).get();
+    }
+}
